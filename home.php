5472d7989fdc8d0029aa891e372efe93e6340615
wajhemuhammad <wajhe@zameen.com>
2014-08-19 15:03:36 +0500

    removed the echos on home.php

--- home.php
+++ home.php
@@ -6,7 +6,3 @@
             <div class="box"> <a href="http://elections.insaf.pk/Endorsements.aspx" class="five"><img style="display: inline-block; width: 100%; z-index: 10;" src="files/box-5-c.jpg" alt="" class="growImage"></a></div>
             <div class="box noRMar"> <a href="http://www.fundyourtsunami.com/" target="_blank" class="six"><img style="display: inline-block; width: 100%; z-index: 10;" src="files/box-6-c.jpg" alt="" class="growImage"></a></div>
   </section>
-  <?
-   echo "new change";
-   echo "some new changes";
-  ?>
